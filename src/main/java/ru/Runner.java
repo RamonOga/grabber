ba927059836c6c140f7b4d3f5a409668f41bae3c
Roman Markelov <Roman.sercent@gmail.com>
2021-03-17 15:50:25 +0300

    trash commit

--- src/main/java/ru/Runner.java
+++ src/main/java/ru/Runner.java
@@ -3,11 +3,16 @@ package ru;
 import ru.db.PsqlStore;
 import ru.parse.SqlRuParse;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 public class Runner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Properties properties = PropertiesCreator.getProperties("app.properties");
         PsqlStore sqlt = new PsqlStore(properties);
         SqlRuParse sqlRuParse = new SqlRuParse(properties);
